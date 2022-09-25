01daa714c5bca99d509cf4e1fdac4cdd963640bb
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-09-24 19:07:18 -0700

    final fix the figher selection issue, user can now click anywhere on image to choose fighter

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //TO FIX: fighter images, they shift up when selected (use stack flexbox or fixed sizes)
-//TO FIX: clicking on the classic fighters, set each element into a button
+//TO FIX: image size being dynamic
 
 var human = new Player("Human","🎸");
 var computer = new Player("Computer","💾");
@@ -11,6 +11,7 @@ var classicFighters = document.querySelector("#classicFighterSection");
 var classicGuitar1 = document.querySelector("#classicGuitar1");
 var classicGuitar2 = document.querySelector("#classicGuitar2");
 var classicGuitar3 = document.querySelector("#classicGuitar3");
+var fighterSelection = document.querySelector("#classicChoice");
 
 classicBttn.addEventListener('click', loadClassicFigherPage);
 classicFighters.addEventListener('click', selectClassicFighter);
@@ -27,23 +28,23 @@ function loadClassicFigherPage(){
 }
 
 function selectClassicFighter(event){
-  console.log(event.target.closest(".rock-image"));
-
-  if (event.currentTarget.classList.contains("rock-image")){
-    console.log("whoa")
-    // myGame.selectHumanFighter("rock");
-    // classicGuitar1.innerText = "🎸";
-    // console.log(myGame);
+  if (event.target.classList.contains("rock-image")){
+    fighterSelection.className = "classic-choice";
+    myGame.selectHumanFighter("rock");
+    classicGuitar1.innerText = "🎸";
+    console.log(myGame);
   }
   else if (event.target.classList.contains("paper-image")){
-    // myGame.selectHumanFighter("paper");
-    // classicGuitar2.innerText = "🎸";
-    // console.log(myGame);
+    fighterSelection.className = "classic-choice";
+    myGame.selectHumanFighter("paper");
+    classicGuitar2.innerText = "🎸";
+    console.log(myGame);
   }
   else if (event.target.classList.contains("scissors-image")){
-    // myGame.selectHumanFighter("scissors");
-    // classicGuitar3.innerText = "🎸";
-    // console.log(myGame);
+    fighterSelection.className = "classic-choice";
+    myGame.selectHumanFighter("scissors");
+    classicGuitar3.innerText = "🎸";
+    console.log(myGame);
   }
 }
 
