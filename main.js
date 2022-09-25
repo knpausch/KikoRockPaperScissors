f3d91bf86375c6b166e495767c9a6a337c8832d0
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-09-24 19:49:44 -0700

    incorporates a timeout so it updates to fighter game page when user selects a fighter

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ var classicGuitar1 = document.querySelector("#classicGuitar1");
 var classicGuitar2 = document.querySelector("#classicGuitar2");
 var classicGuitar3 = document.querySelector("#classicGuitar3");
 var fighterSelection = document.querySelector("#classicChoice");
+var fightScene = document.querySelector("#fightGameSection");
+var fighter1 = document.querySelector("#fighter1Image");
 
 classicBttn.addEventListener('click', loadClassicFigherPage);
 classicFighters.addEventListener('click', selectClassicFighter);
@@ -27,24 +29,37 @@ function loadClassicFigherPage(){
   console.log(myGame);
 }
 
+function loadFightPage(){
+  classicFighters.className = "classic-game-section-hidden";
+  fightScene.className = "fight-game-section";
+}
+
 function selectClassicFighter(event){
   if (event.target.classList.contains("rock-image")){
     fighterSelection.className = "classic-choice";
     myGame.selectHumanFighter("rock");
     classicGuitar1.innerText = "🎸";
     console.log(myGame);
+    fighter1.src = "assets/rock.png";
+    const myTimeout = setTimeout(loadFightPage, 3000);
+
   }
   else if (event.target.classList.contains("paper-image")){
     fighterSelection.className = "classic-choice";
     myGame.selectHumanFighter("paper");
     classicGuitar2.innerText = "🎸";
     console.log(myGame);
+    fighter1.src = "assets/paper.png";
+    const myTimeout = setTimeout(loadFightPage, 3000);
+
   }
   else if (event.target.classList.contains("scissors-image")){
     fighterSelection.className = "classic-choice";
     myGame.selectHumanFighter("scissors");
     classicGuitar3.innerText = "🎸";
     console.log(myGame);
+    fighter1.src = "assets/scissors.png";
+    const myTimeout = setTimeout(loadFightPage, 3000);
   }
 }
 
